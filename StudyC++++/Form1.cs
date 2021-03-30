9c9423cffb7ae0f1e45685c8810e58a65274e7ef
grady1218 <isikawaseizi810@gmail.com>
2021-03-30 22:38:10 +0900

    プロジェクト ファイルを追加します。

---
+++ StudyC++++/Form1.cs
@@ -0,0 +1,58 @@
+﻿#define DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace StudyC____
+{
+    class Form1 : Form
+    {
+        public Form1()
+        {
+            Test();
+        }
+
+        [Conditional("DEBUG")]
+        public void Test()
+        {
+            var count = 0;
+            var date = DateTime.Now;
+
+            List<int> timeList = new List<int>();
+
+            Console.WriteLine( DateTime.Now );
+
+            var t = new Timer
+            {
+                Interval = 13,
+                Enabled  = true
+            };
+
+            t.Tick += (object sender, EventArgs e) => {
+                if(count < 500)
+                {
+                    Parallel.For( 0, 1000, n => { 
+                        timeList.Add((DateTime.Now - date).Milliseconds);
+                        count++;
+                        Console.WriteLine( count );
+                    } );
+                }
+                else
+                {
+                    t.Stop();
+                    Console.WriteLine( timeList.Count( ( v ) => v < 13 ) );
+                }
+                date = DateTime.Now;
+
+            };
+
+
+        }
+    }
+}
